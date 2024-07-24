856eea59dd7d2e626775e37aae9fbbe976529d71
Luca <luca.lu@oocl.com>
2024-07-24 10:00:29 +0800

    home work

--- openai-translator/README-CN.md
+++ openai-translator/README-CN.md
@@ -86,8 +86,8 @@ python ai_translator/main.py
 
 ```bash
 # 将您的 api_key 设置为环境变量
-export OPENAI_API_KEY="sk-xxx"
-python ai_translator/main.py --model_type OpenAIModel --openai_api_key $OPENAI_API_KEY --file_format markdown --book tests/test.pdf --openai_model gpt-3.5-turbo
+set OPENAI_API_KEY=
+python ai_translator/main.py --model_type OpenAIModel --openai_api_key %OPENAI_API_KEY% --file_format markdown --book tests/test.pdf --openai_model gpt-3.5-turbo
 ```
 
 这是使用 GLM 模型的例子：
--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -17,11 +17,11 @@ if __name__ == "__main__":
     model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
     model = OpenAIModel(model=model_name, api_key=api_key)
-
+    tagert_language = args.target_language
 
     pdf_file_path = args.book if args.book else config['common']['book']
     file_format = args.file_format if args.file_format else config['common']['file_format']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format,tagert_language)
